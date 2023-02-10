7b589cca1f0b5a6913fb96b33d5a9873a9b2873d
Batı Gökovalı <batigokovali@icloud.com>
2023-02-10 18:26:47 +0300

    done

--- src/Components/MyLocation.jsx
+++ src/Components/MyLocation.jsx
@@ -1,13 +1,29 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { FaBeer } from 'react-icons/fa';
+import { BsFillSunFill, BsCloudsFill, BsFillCloudRainFill, BsSnow2, BsFillCloudSunFill } from 'react-icons/bs'
 
 const MyLocation = (props) => {
 
     return (
-        <Container>
-            <h1>{props.data.name}</h1>
-            <h1>Average Temp: {props.data.main.temp}</h1>
-            <h3>Feels Like: {props.data.main.feels_like}</h3>
+        <Container className="display-weather py-5">
+            <h1>{props.data.name}, {props.data.sys.country}</h1>
+            <h1>{props.data.main.temp}°C</h1>
+            <h3>Feels Like: {props.data.main.feels_like}°C, {props.data.weather[0].description}
+                {props.data.weather[0].main === "Clear" && (<BsFillSunFill className="ml-2"></BsFillSunFill>)}
+                {props.data.weather[0].main === "Clouds" && (<BsCloudsFill className="ml-2"></BsCloudsFill>)}
+                {props.data.weather[0].main === "Rain" && (<BsFillCloudRainFill className="ml-2"></BsFillCloudRainFill>)}
+                {props.data.weather[0].main === "Snow" && (<BsSnow2 className="ml-2"></BsSnow2>)}
+                {props.data.weather[0].main === "Half Cloud" && (<BsFillCloudSunFill className="ml-2"></BsFillCloudSunFill>)}
+            </h3>
+            <Row>
+                <Col xs={6}>
+                    <h3>Wind Degree: {props.data.wind.deg}</h3>
+                    <h3>Wind Speed: {props.data.wind.speed}</h3>
+                </Col>
+                <Col xs={6}>
+                    <h3>Humidity: {props.data.main.humidity}%</h3>
+                    <h3>Pressure: {props.data.main.pressure}hPa</h3>
+                </Col>
+            </Row>
         </Container>
 
     );
--- src/Components/MySearch.jsx
+++ src/Components/MySearch.jsx
@@ -3,8 +3,8 @@ import { Container, Row, Col, Form } from "react-bootstrap";
 import MyLocation from "./MyLocation";
 
 const MySearch = () => {
-  const [query, setQuery] = useState("");
-  const [locations, setLocation] = useState([])
+  const [query, setQuery] = useState();
+  const [locations, setLocation] = useState(null)
 
   const url =
     `https://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=56db6901d4a5b12ceea085dbc13358d4&units=metric`;
@@ -33,10 +33,10 @@ const MySearch = () => {
   return (
     <Container fluid>
       <Row>
-        <Col className="mr-auto mb-5 jumbotron first-header">
+        <Col className="mr-auto mb-0 jumbotron first-header">
           <h1 className="header">Search for a Location!</h1>
           <Row>
-            <Col xs={4} className="mx-auto px-0 mt-5">
+            <Col xs={6} className="mx-auto px-0 mt-5">
               <Form onSubmit={handleSubmit}>
                 <Form.Control
                   type="search"
@@ -51,8 +51,8 @@ const MySearch = () => {
         </Col>
       </Row>
       <Row>
-        <Col className="mt-5">{locations.length ? (<MyLocation data={locations}></MyLocation>) : (
-          <h2>There are no cities selected</h2>
+        <Col className="mt-5">{locations ? (<MyLocation data={locations}></MyLocation>) : (
+          <h2>Result will be displayed here...</h2>
         )}
         </Col>
       </Row>
